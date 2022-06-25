984a0fe40edf56935b034203bd88bddcea7a6202
Ronny Delgado <delgado.ronny@gmail.com>
2022-06-24 21:13:35 -0600

    Finished input component for the Feed area

--- components/feed/index.tsx
+++ components/feed/index.tsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Input from "./Input";
 
 export default function Index() {
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <Header />
+      <Input />
     </div>
   );
 }
