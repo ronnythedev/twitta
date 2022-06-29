8c7f436df57a9edde7e23963fed999b9ec112758
Ronny Delgado <delgado.ronny@gmail.com>
2022-06-29 11:21:26 -0600

    Add secret to the env variables and in nexauth component

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,13 +6,14 @@ type ExtendedUserType = User & {username?:string; uid?:string};
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? "",
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
   pages: {
     signIn: "/auth/signIn",
   },
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     async session({ session, token }) {
       // This is made-up username since we are not using actual Twitter or Twitter API whatsoever
